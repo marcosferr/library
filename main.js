e8ae435c84a4eaa67c221a20188fb1c2c5f26ee8
marcosferr <ferreiramarcosadrian@gmail.com>
2023-06-19 12:43:05 -0400

    Basic working version

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
+//Book list
+const bookList = document.getElementById('bookList')
 
 function getFieldsValues() {
     let newBookValues = {}
     newBookValues['title'] = title.value;
     newBookValues['author'] = author.value;
     newBookValues['pages'] = pages.value;
-    newBookValues['read'] = read.value;
+    newBookValues['read'] = read.checked;
     return newBookValues
 }
 
@@ -30,14 +32,36 @@ function Book(title, author, pages, read) {
         return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read ' : 'not read yet'}`)
     }
 }
-Book.prototype.toggleRead = () => { this.read = !read }
+Book.prototype.toggleRead = function () {
+    this.read = !this.read
+}
+function toggleBookStatus(index) {
+    myLibrary[index].toggleRead()
+    renderBooks()
+}
 function addBookToLibrary() {
     const newBookData = getFieldsValues();
     const newBook = new Book(newBookData.title, newBookData.author, newBookData.pages, newBookData.read)
     myLibrary.push(newBook)
     console.log(myLibrary)
+    renderBooks()
 }
-
-
-
-console.log(theHobbit.info())
+function deleteBook(index) {
+    myLibrary.splice(index, 1)
+    renderBooks()
+}
+function renderBooks() {
+    const booksData = myLibrary.reduce((total, el, i) => {
+        return (total += `<div class='card' id=${i}> 
+        <span> Title: ${el.title} </span><br/>
+        <span> Author: ${el.author} </span><br/>
+        <span> Pages: ${el.pages} </span><br/>
+        <button onClick='toggleBookStatus(${i})'>  ${el.read ? 'read' : 'not read yet'} </button> <br> 
+       
+        <button onClick='deleteBook(${i})'>Eliminar Libro </button>
+        <div>`)
+    }, '')
+    bookList.innerHTML = ''
+    bookList.innerHTML = booksData
+    console.log(booksData)
+}
